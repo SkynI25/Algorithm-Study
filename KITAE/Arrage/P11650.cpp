ad30ad4b341c2787fb1f9fa3dff2aac7bb2b847a
Ki Tae Park <qkrrlxo92@koreatech.ac.kr>
2018-07-09 12:34:05 +0900

    Create P11650.cpp
    [URI] https://www.acmicpc.net/problem/11650
    [설명] vector와 pair STL을 사용하면서 해결할 수 있었음. 그리고 sort() 알고리즘을 사용하니 알아서 문제에서 제시한 대로 정렬이 되었음.
    중요한 것은 sort 알고리즘을 직접 짜보는 것이겠지..이번 문제에선 vector에서 pair 를 사용하는 방법을 익힐 수 있었음, 그리고 ios~~ 를 사용해보며 입력의 속도를 높이는 노력을 기하고 변형 for문을 사용해봄.

---
+++ KITAE/Arrage/P11650.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <utility>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+
+	vector<pair<int, int>> arr;
+	int N, a, b;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> a >> b;
+		arr.push_back(make_pair(a, b));
+	}
+
+	sort(arr.begin(), arr.end());
+
+	for (auto obj : arr) {
+		cout << obj.first << " " << obj.second << '\n';
+	}
+
+	system("pause");
+
+	return 0;
+}
