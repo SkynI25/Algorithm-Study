3cdc4f14a98a22a2a3a684c3005b34dd76a07106
Ki Tae Park <qkrrlxo92@koreatech.ac.kr>
2018-07-11 16:04:31 +0900

    Create P11652.cpp
    [URI] https://www.acmicpc.net/problem/11652
    [설명] 이전 문제와 같이 아이디어를 요구하는 문제. 어찌보면 정렬 알고리즘을 잘 짤 수 있는 가를 물어보는 핵심 문제중 하나.
    입력받은 수중 중복횟수가 가장 큰 값을 출력해야 한다, 만약 중복횟수가 숫자마다 비슷하다면 그 중 가장 작은 숫자를 출력하면 됨.
    핵심은 여러개의 숫자를 입력받고 정렬을 해준 뒤 앞 뒤 비교를 하면서 cnt를 증가시킬 지 1로 초기화 할지 둔다. 같으면 cnt 증가, 다르면 cnt=1로 초기화. 그리고 ans_cnt를 통해 가장 큰 cnt 값을 저장하면서 가장 큰 cnt를 가진 숫자를 찾기위해 N번만큼 순회한다. 이때 찾으면 그 때의 숫자를 ans에 대입하고 정답을 출력하면 끝.
    a 를 int로 해줘서 X을 싼 거 뺴곤 문제가 없었다.거기서 좀 헤매긴 했지만 ㅎ

---
+++ KITAE/Arrage/P11652.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    
+	int N;
+	long long a;
+	int cnt = 1;
+	int ans_cnt = 1;
+	
+	cin >> N;
+
+	vector<long long> arr;
+
+	for (int i = 0; i < N; i++) {
+		cin >> a;
+		arr.push_back(a);
+	}
+
+	sort(arr.begin(), arr.end());
+
+	long long ans = arr[0];
+
+	for (int i = 1; i < N; i++) {
+		if (arr[i] == arr[i - 1]) {
+			cnt++;
+		}
+		else {
+			cnt = 1;
+		}
+		if (ans_cnt < cnt) {
+			ans_cnt = cnt;
+			ans = arr[i];
+		}
+	}
+
+	cout << ans;
+
+	return 0;
+}
